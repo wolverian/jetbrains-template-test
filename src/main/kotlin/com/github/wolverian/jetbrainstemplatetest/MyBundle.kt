71d5cc9ab40132e8370ba2d69b8b2e648dca23f4
GitHub Action <action@github.com>
2020-08-20 11:33:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/wolverian/jetbrainstemplatetest/MyBundle.kt
+++ src/main/kotlin/com/github/wolverian/jetbrainstemplatetest/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.wolverian.jetbrainstemplatetest
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/wolverian/jetbrainstemplatetest/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.wolverian.jetbrainstemplatetest.services
+
+import com.github.wolverian.jetbrainstemplatetest.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
